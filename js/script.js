18aab9a449fc43294e90e811d993a1c40afffe54
ADSM <yesform2018@gmail.com>
2022-03-01 13:36:50 +0600

    realease date erorr completed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const searceInput = document.getElementById('searce-input');
-// console.log(searceInput.value)
+const notFoundErorr = style => {
+    document.getElementById('not-found-erorr').style.display = style;
+};
+notFoundErorr('none');
 
 const searceBtn = () => {
     const url = (`https://openapi.programming-hero.com/api/phones?search=${searceInput.value}`);
@@ -11,20 +14,26 @@ const searceBtn = () => {
 };
 const loadPhones = phones => {
     const parentDiv = document.getElementById('parent');
+    if (phones.length <= 0) {
+        notFoundErorr('block');
+    }
+    else {
+        notFoundErorr('none');
+    }
     phones.forEach(phone => {
         const div = document.createElement('div');
+        div.className = 'col col-12 col-md-4';
         div.innerHTML = `
-        <div class="d-flex mx-auto mb-3 py-4 pe-2 rounded-3 shadow-lg phone" style="width: 18rem;">
-                <img src="${phone.image}" class="card-img-top ms-2" alt="...">
-                <div class="card-body p-3">
-                    <h5 class="card-title">${phone.brand}</h5>
-                    <p class="card-text">${phone.phone_name}</p>
-                    <button onclick="loadDetail('${phone.slug}')" class="btn btn-dark">Details</button>
-                </div>
+    <div class="mb-3 p-3 rounded-3 shadow-lg phone" style="width: 18rem;">
+            <img src="${phone.image}" class="card-img-top w-75" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${phone.brand}</h5>
+                <p class="card-text">${phone.phone_name}</p>
+                <button onclick="loadDetail('${phone.slug}')" class="btn btn-dark">Details</button>
             </div>
-        `;
+        </div>
+    `;
         parentDiv.appendChild(div);
-        // console.log(phone.slug)
     });
 };
 const parentDiv = document.getElementById('detail-container');
@@ -36,20 +45,33 @@ const loadDetail = (details) => {
     parentDiv.innerHTML = '';
 };
 const displayDetail = detail => {
+    let releaseErorr = '';
+    if (detail.releaseDate == '') {
+        releaseErorr = 'Realease date not found!';
+    }
+    else {
+        releaseErorr = '';
+    }
     const div = document.createElement('div');
     div.innerHTML = `
-    <div class="d-flex flex-column w-100 mx-auto m-2 p-3 rounded-3 shadow-lg phone" style="width: 18rem;">
+    <div class="d-flex flex-column w-75 mx-auto p-5 rounded-3 shadow-lg phone" style="width: 18rem;">
             <img src="${detail.image}" class="w-25" alt="...">
             <div class="card-body p-3">
                 <h5 class="card-title">Brand: ${detail.brand} , Name: ${detail.name}</h5>
                 <p class="card-text"></p>
                 <p class="card-text">Display Size: ${detail.mainFeatures.displaySize}</p>
                 <p class="card-text">Memory: ${detail.mainFeatures.memory}</p>
-                <p class="card-text">Release Date: ${detail.releaseDate}</p>
+                <p class="card-text release">Release: ${detail.releaseDate} ${releaseErorr}</p>
                 <p class="card-text">Sensores: ${detail.mainFeatures.sensors}</p>
+                <p class="card-text">Bluetooth: ${detail.others.Bluetooth}</p>
+                <p class="card-text">GPS: ${detail.others.GPS}</p>
+                <p class="card-text">NFC: ${detail.others.NFC}, Radio ${detail.others.Radio}</p>
+                <p class="card-text">USB: ${detail.others.USB}</p>
+                <p class="card-text">WLAN: ${detail.others.WLAN}</p>
             </div>
         </div>
     `;
     parentDiv.appendChild(div)
-    console.log(detail)
+
+    // console.log(detail)
 };
